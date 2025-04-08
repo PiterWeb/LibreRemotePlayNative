0fce0b8c7f4a0b3cc7ebfe9916dc173b98985a64
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2025-04-08 21:40:32 +0200

    minimal example

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"image"
-	"image/jpeg"
 	"os"
-	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -19,56 +15,17 @@ func main() {
 	w := a.NewWindow("Hello World")
 	w.Resize(fyne.Size{Width: 1280, Height: 720})
 
-	imgReader := new(bufio.Reader)
-	imgWriter := new(bufio.Writer)
-
-	imgReaderWriter := bufio.NewReadWriter(imgReader, imgWriter)
-
-	img, err := os.ReadFile("./example.png")
+	img, err := getImageFromFilePath("./example.png")
 
 	if err != nil {
 		return
 	}
 
-	_, _ = imgReaderWriter.Write(img)
-
-	view := container.NewAdaptiveGrid(1, canvas.NewImageFromReader(imgReaderWriter, "main_image"))
+	view := container.NewAdaptiveGrid(1, canvas.NewImageFromImage(img))
 
 	w.SetContent(view)
 
-	go updateView(view, imgWriter)
-
 	w.ShowAndRun()
-}
-
-func updateView(view *fyne.Container, imgWriter *bufio.Writer) {
-
-	if canvasImg, ok := view.Objects[0].(*canvas.Image); ok {
-		for range time.Tick(time.Second) {
-
-			if canvasImg.Image == image.Black {
-				ex_img, err := os.ReadFile("./example.png")
-
-				if err != nil {
-					continue
-				}
-
-				_, _ = imgWriter.Write(ex_img)
-			} else {
-				buf := new(bytes.Buffer)
-				err := jpeg.Encode(buf, image.Black, nil)
-
-				if err != nil {
-					continue
-				}
-
-				_, _ = imgWriter.Write(buf.Bytes())
-			}
-
-			canvasImg.Refresh()
-
-		}
-	}
 }
 
 func getImageFromFilePath(filePath string) (image.Image, error) {
